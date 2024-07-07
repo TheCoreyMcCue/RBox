e00d5d73eda326e9ae9c9a6d129918bfc6479a30
Corey McCue <coreysmccue@gmail.com>
2024-07-07 23:19:17 +0200

    read user recipes functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,10 +3,38 @@ import { getRecipesByUser } from "@/lib/actions/recipe.action";
 
 const Dashboard = async () => {
   const user = await currentUser();
-  const recipes = await getRecipesByUser(user.id);
-  console.log(user?.id, "recipes", recipes);
 
-  return <div className="min-h-[90vh]">user</div>;
+  if (!user) {
+    // Handle the case where the user is not authenticated
+    return (
+      <div className="min-h-[90vh]">
+        <h1 className="text-2xl font-bold mb-4">
+          Please sign in to view your recipes
+        </h1>
+      </div>
+    );
+  }
+
+  const recipes = await getRecipesByUser(user.id);
+  console.log(user.id, "recipes", recipes);
+
+  return (
+    <div className="min-h-[90vh]">
+      <h1 className="text-2xl font-bold mb-4">Your Recipes</h1>
+      {recipes.length > 0 ? (
+        <ul>
+          {recipes.map((recipe) => (
+            <li key={recipe._id}>
+              <h2 className="text-xl font-semibold">{recipe.title}</h2>
+              <p>{recipe.description}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No recipes found.</p>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
