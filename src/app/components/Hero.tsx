727e284f496acf94d6ea27f3a80b3ae54cc96075
Corey McCue <coreysmccue@gmail.com>
2024-07-07 14:09:03 +0200

    update hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,14 +8,14 @@ const Hero = () => {
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
         <div className="md:w-1/2 text-center md:text-left">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Welcome to My Website
+            Welcome to AreBox
           </h1>
           <p className="text-lg md:text-xl mb-8">
-            We provide the best services to help you succeed.
+            We help you pick whats for dinner tonight.
           </p>
           <Link href="/services">
             <div className="inline-block bg-blue-500 text-white py-3 px-6 rounded-full text-lg font-semibold hover:bg-blue-600 transition duration-300 cursor-pointer">
-              Our Services
+              Get Started
             </div>
           </Link>
         </div>
