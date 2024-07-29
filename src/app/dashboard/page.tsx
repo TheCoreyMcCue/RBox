b309ab25b5f5924571d7e62e72c14f674fcf147d
Corey McCue <coreysmccue@gmail.com>
2024-07-29 06:01:30 +0200

    dashboard tweaks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -32,7 +32,6 @@ const Dashboard = () => {
         } catch (error) {
           console.error("Error fetching recipes:", error);
         } finally {
-          // setLoading(false);
         }
       }
       setLoading(false);
@@ -44,41 +43,29 @@ const Dashboard = () => {
   if (loading) {
     return (
       <div className="min-h-[90vh] flex flex-col items-center justify-center">
-        <div role="status">
-          <svg
-            aria-hidden="true"
-            className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="currentColor"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
-          <span className="sr-only">Loading...</span>
+        <div className="flex items-center justify-center space-x-2">
+          <div className="w-4 h-4 rounded-full bg-blue-500 animate-bounce"></div>
+          <div className="w-4 h-4 rounded-full bg-blue-600 animate-bounce delay-150"></div>
+          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce delay-300"></div>
         </div>
+        <span className="mt-4 text-gray-600 text-lg">Loading Recipes...</span>
       </div>
     );
   }
 
   if (!isSignedIn) {
     return (
-      <div className="bg-gray-900 min-h-[90vh] text-white py-20">
-        <div className="container mx-auto px-4 flex flex-col md:flex-row items-center">
-          <div className="md:w-1/2 text-center md:text-left">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4">
-              New to Our Box?
-            </h1>
-            <p className="text-lg md:text-xl mb-8">Sign in to get started!</p>
-
-            <div className="inline-block bg-blue-500 text-white py-3 px-6 rounded-full text-lg font-semibold hover:bg-blue-600 transition duration-300 cursor-pointer">
-              <SignInButton>Sign Up</SignInButton>
+      <div className="bg-gradient-to-br from-red-500 via-white-500 to-blue-600 min-h-[90vh] text-white flex flex-col items-center justify-center">
+        <div className="text-center max-w-xl p-8 bg-white/10 backdrop-blur-md rounded-lg shadow-2xl">
+          <h1 className="text-5xl md:text-6xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-orange-500">
+            Welcome to Our Box!
+          </h1>
+          <p className="text-lg md:text-xl mb-6 font-light">
+            Sign in to discover and manage your favorite recipes effortlessly.
+          </p>
+          <div className="inline-block">
+            <div className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-3 px-10 rounded-full text-lg font-semibold hover:from-purple-600 hover:to-blue-500 transition-all duration-300 ease-in-out transform hover:scale-105 shadow-lg cursor-pointer">
+              <SignInButton>Sign Up Now</SignInButton>
             </div>
           </div>
         </div>
@@ -102,7 +89,7 @@ const Dashboard = () => {
           {recipes.map((recipe) => (
             <div key={recipe._id}>
               <Link href={`/recipes/${recipe._id}`}>
-                <div className="block bg-white shadow-lg rounded-lg overflow-hidden transform transition-transform hover:scale-105 cursor-pointer">
+                <div className="block bg-white shadow-lg rounded-2xl overflow-hidden transform transition-transform hover:scale-105 cursor-pointer">
                   <Image
                     src={recipe.image || Placeholder}
                     alt={recipe.title}
