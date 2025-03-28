a85123867df4b459eeda2adf10ed1a39122bc388
thecoreymccue <corey.mccue@datadoghq.com>
2025-03-28 16:50:14 +0100

    begin refactor

---
+++ src/app/components/NotSigned.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { SignInButton } from "@clerk/nextjs";
+
+const NotSigned = () => {
+  return (
+    <div className="bg-gradient-to-br from-red-500 via-white-500 to-blue-600 min-h-[90vh] text-white flex flex-col items-center justify-center">
+      <div className="text-center max-w-xl p-8 bg-white/10 backdrop-blur-md rounded-lg shadow-2xl">
+        <h1 className="text-5xl md:text-6xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-orange-500">
+          Welcome to Recipe Nest!
+        </h1>
+        <p className="text-lg md:text-xl mb-6 font-light">
+          Sign in to discover and manage your favorite recipes effortlessly.
+        </p>
+        <div className="inline-block">
+          <div className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-3 px-10 rounded-full text-lg font-semibold hover:from-purple-600 hover:to-blue-500 transition-all duration-300 ease-in-out transform hover:scale-105 shadow-lg cursor-pointer">
+            <SignInButton>Sign Up Now</SignInButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotSigned;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { Recipe } from "../utils/types";
 import Placeholder from "../../../public/placeholder.png";
 import LoadingScreen from "../components/LoadingScreen";
+import NotSigned from "../components/NotSigned";
 
 import { lasagnaRecipe } from "./exampleRecipe";
 
@@ -88,25 +89,7 @@ const Dashboard = () => {
 
   if (loading) return <LoadingScreen />;
 
-  if (!isSignedIn) {
-    return (
-      <div className="bg-gradient-to-br from-red-500 via-white-500 to-blue-600 min-h-[90vh] text-white flex flex-col items-center justify-center">
-        <div className="text-center max-w-xl p-8 bg-white/10 backdrop-blur-md rounded-lg shadow-2xl">
-          <h1 className="text-5xl md:text-6xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-orange-500">
-            Welcome to Recipe Nest!
-          </h1>
-          <p className="text-lg md:text-xl mb-6 font-light">
-            Sign in to discover and manage your favorite recipes effortlessly.
-          </p>
-          <div className="inline-block">
-            <div className="bg-gradient-to-r from-blue-500 to-purple-600 text-white py-3 px-10 rounded-full text-lg font-semibold hover:from-purple-600 hover:to-blue-500 transition-all duration-300 ease-in-out transform hover:scale-105 shadow-lg cursor-pointer">
-              <SignInButton>Sign Up Now</SignInButton>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  if (!isSignedIn) return <NotSigned />;
 
   return (
     <div className="min-h-[90vh] container mx-auto px-4 py-8 relative">
