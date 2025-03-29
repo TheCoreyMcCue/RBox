7c7bd63ddfa71732fe6c2f6a2c0b6c8491a99e6b
thecoreymccue <corey.mccue@datadoghq.com>
2025-03-29 20:42:42 +0100

    small config change

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,28 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "thewoksoflife.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "localhost:3000",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "**",
+        pathname: "/**",
       },
     ],
   },
